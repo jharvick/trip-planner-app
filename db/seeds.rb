343bdc7d541752823f01bb505aa78ad44fd9150e
Jared Harvick <jharvick38@gmail.com>
2023-06-17 08:12:45 -0500

    Changed password_digest in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: "Jared", email: "jared@test.com", password_digest: "password")
-
-
+User.create(name: "Jared", email: "jared@test.com", password: "password")
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "create" do
-    post "/users.json", params: { name: "Test", email: "test@test.com", password_digest: "password", password_confirmation: "password" }
+    post "/users.json", params: { name: "Test", email: "test@test.com", password_digest: "password" }
     post "/sessions.json", params: { email: "test@test.com", password: "password" }
     assert_response 201
 
